fc5168256f0d8a56b269eeed5578f6eb8fdd98e1
futuresavecooperative-website <futuresavecooperative@gmail.com>
2026-01-21 14:27:28 +0100

    Update index.js Fix WhatsApp link + hero operated by text

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   );
 
   // ✅ MUST use backticks here
-  const whatsappLink = `https://wa.me/2347068967636?text=${whatsappMessage}`;
+  const whatsappLink = {https://wa.me/2347068967636?text=${whatsappMessage}};
   return (
     <main style={{ padding: 40, fontFamily: "Arial" }}>
       {/* HERO SECTION */}
